b7e4eb4b00443c3f1fdffe06785d76323e28f110
ssumanaxioned <sumans@axioned.com>
2023-11-23 02:11:48 +0530

    WIP

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'www.orbis.com.ar',
+          port: '',
+          pathname: '/wp-content/themes/barberry/images/**',
+        },
+      ],
+    },
+}
 module.exports = nextConfig
